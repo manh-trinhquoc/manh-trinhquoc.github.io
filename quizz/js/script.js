783cdbd7f1445a43386e71b94795332fb27bb218
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-05-14 11:55:58 +0700

    save changes after a working session

--- quizz/js/script.js
+++ quizz/js/script.js
@@ -31,25 +31,12 @@ let q5 = new Quizz("Thẻ <input type=”Password” …> dùng để làm gì?"
 
 
 
-function randomSuffleArr(arr) {
-    // Hàm đảo thứ tự ngẫu nhiên 1 chuỗi.
-    console.group("randomSuffleArr");
-    let nArr = arr.slice();
-    let result = [];
-    let count = 0;
-    while (nArr.length > 0) {
-        let random = Math.floor(Math.random() * nArr.length);
-        let item = nArr.splice(random, 1);
-        result.push(...item);
-    }
-    console.groupEnd();
-    return result;
-}
 
-// Bước 2: Tạo đối tượng là các nút bấm
+
+// Bước 2: Tạo đối tượng là các nút bấm và hàm onclick
 function Btn(value, onclick = function() {
     // console.log(this.value);
-    quizzInProgress(this.value);
+    quizzInProgress(this);
 }) {
 
     this.value = value;
@@ -60,6 +47,28 @@ let btnA = new Btn("a");
 let btnB = new Btn("b");
 let btnC = new Btn("c");
 let btnD = new Btn("d");
+// Test
+// btnA.onclick();
+// btnA.onclick();
+// btnA.onclick();
+// btnA.onclick();
+// btnA.onclick();
+
+let btnReview = new Btn("review");
+btnReview.onclick = function() {
+    // console.log(this.value);
+    console.trace();
+    finished(undefined, this);
+}
+// Test
+// btnReview.onclick();
+
+let btnReplay = new Btn("replay");
+btnReplay.onclick = function() {
+    quizzInProgress(this);
+}
+// Test
+// btnReplay.onclick();
 
 
 document.getElementsByClassName("js-answer__a")[0].onclick = function() {
@@ -75,58 +84,149 @@ document.getElementsByClassName("js-answer__c")[0].onclick = function() {
 document.getElementsByClassName("js-answer__d")[0].onclick = function() {
     btnD.onclick();
 }
+document.getElementsByClassName("js-review")[0].onclick = function() {
+    btnReview.onclick();
+}
+document.getElementsByClassName("js-replay")[0].onclick = function() {
+    btnReplay.onclick();
+}
 
 // Bước 3: xử lý các trạng thái của chương trình
 // Các trạng thái của chương trình: welcome, quizzInProgress, finish, review, trophy
-let programStage = {
-    allStages: ["welcome", "quizzInProgress", "finish", "review", "trophy"],
-    currentStage: "quizzInProgress"
+let viewManager = {
+    allStages: ["welcome", "quizzInProgress", "finished", "reviewing", "trophy"],
+    currentStage: "quizzInProgress",
+    manageView: function(currentStage) {
+        this.currentStage = currentStage;
+        switch (this.currentStage) {
+            case "quizzInProgress":
+                document.getElementsByClassName("js-quizz-in-progress")[0].style.height = "auto";
+                document.getElementsByClassName("finished__wrapper")[0].style.height = 0;
+                break;
+            default:
+                document.getElementsByClassName("js-quizz-in-progress")[0].style.height = "auto";
+                document.getElementsByClassName("finished__wrapper")[0].style.height = "auto";
+        }
+    }
 }
 let quizzInProgress = (function() {
-    // this[programStage.currentStage] = (function() {
     // Hàm thực hiện các thao tác khi đang trong trạng thái làm quizz 
-    console.group("quizzInProgress");
-    qArr = [q1, q2, q3, q4, q5];
-    qArr = randomSuffleArr(qArr);
     let qAnswer = [];
     let userAnswerArr = [];
-    let question = qArr.pop();
-    showQuizz(question);
+    let question = [];
+    initial();
     return function(btn) {
+
+        console.log(btn.value);
+        if (btn.value == "replay") {
+            initial();
+            return;
+        }
         console.log(qArr.length);
-        if (programStage.currentStage == "quizzInProgress") {
-            userAnswerArr.push({ question, btn })
+        if (viewManager.currentStage == "quizzInProgress") {
+            let value = btn.value
+            userAnswerArr.push({ question, qAnswer, userAnser: btn.value })
+            // userAnswerArr.push({ question: question, qAnswer: qAnswer, value: value })
             question = qArr.pop();
         }
-        if (question != undefined)
-            showQuizz(question);
-        else {
-            programStage.currentStage = "finish";
-            finish(userAnswerArr);
+        if (question != undefined) {
+            qAnswer = [question.a, question.b, question.c, question.d];
+            // console.log(qAnswer);
+            qAnswer = randomSuffleArr(qAnswer);
+            showQuizz(question, qAnswer);
+        } else {
+            // viewManager.currentStage = "finish";
+            finished(userAnswerArr);
         }
         console.log({ userAnswerArr });
+
+
     }
 
-    function showQuizz(question) {
-        let qAnswer = [question.a, question.b, question.c, question.d];
-        // console.log(qAnswer);
-        qAnswer = randomSuffleArr(qAnswer);
-        console.log({ qAnswer });
+    function showQuizz(question, qAnswer) {
+        // console.log({ qAnswer });
         document.getElementsByClassName("js-question")[0].textContent = question.q;
         document.getElementsByClassName("js-answer__a")[0].textContent = qAnswer[0];
         document.getElementsByClassName("js-answer__b")[0].textContent = qAnswer[1];
         document.getElementsByClassName("js-answer__c")[0].textContent = qAnswer[2];
         document.getElementsByClassName("js-answer__d")[0].textContent = qAnswer[3];
     }
+
+    function initial() {
+
+        viewManager.manageView("quizzInProgress");
+        console.group("quizzInProgress");
+        qArr = [q1, q2, q3, q4, q5];
+        qArr = [q1];
+        qArr = randomSuffleArr(qArr);
+        qAnswer = [];
+        userAnswerArr = [];
+        question = qArr.pop();
+        qAnswer = [question.a, question.b, question.c, question.d];
+        // console.log(qAnswer);
+        qAnswer = randomSuffleArr(qAnswer);
+        showQuizz(question, qAnswer);
+    }
+
+    function randomSuffleArr(arr) {
+        // Hàm đảo thứ tự ngẫu nhiên 1 chuỗi.
+        console.group("randomSuffleArr");
+        let nArr = arr.slice();
+        let result = [];
+        let count = 0;
+        while (nArr.length > 0) {
+            let random = Math.floor(Math.random() * nArr.length);
+            let item = nArr.splice(random, 1);
+            result.push(...item);
+        }
+        console.groupEnd();
+        return result;
+    }
     console.groupEnd();
 })();
 
-function finish(userAnswerArr, btn) {
-    document.getElementsByClassName("js-quizz-in-progress")[0].style.display = "none";
-}
+function finished(userAnswerArr, btn = undefined) {
+    this["replay"] = function() {
 
-btnA.onclick();
-btnA.onclick();
-btnA.onclick();
-btnA.onclick();
-btnA.onclick();
+    }
+    this["review"] = function() {
+
+    }
+    if (btn == undefined) {
+        viewManager.manageView("finished");
+        this.userAnswerArr = userAnswerArr;
+        let countRightAnswer = 0;
+        for (each of this.userAnswerArr) {
+            // console.log(each.question);
+            // console.log(each.qAnswer);
+            // console.log(each.userAnser);
+            let userAnser = convertUserAnswer(each.qAnswer, each.userAnser);
+            let answerTrue = each.question[each.question.answer]
+            console.log({ userAnser, answerTrue })
+            if (userAnser == answerTrue) {
+                countRightAnswer++;
+            }
+
+        }
+        if (countRightAnswer == this.userAnswerArr.length) {
+            console.log("Go to trophy page")
+        }
+        document.getElementsByClassName("js-result")[0].textContent = `Bạn đã trả lời đúng ${countRightAnswer}
+        trên ${userAnswerArr.length} câu hỏi`
+    } else this[btn.value]();
+
+    function convertUserAnswer(qAnswer, userAnser) {
+        switch (userAnser) {
+            case "a":
+                return qAnswer[0];
+            case "b":
+                return qAnswer[1];
+            case "c":
+                return qAnswer[2];
+            case "d":
+                return qAnswer[3];
+            default:
+                return undefined;
+        }
+    }
+}
