e6293b8f9941a50016b0a40122822dcfb68b6f4b
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-05-25 02:38:56 +0700

    laza: b├ái vß╗ü nh├á buß╗òi 19

--- laza/js/products.js
+++ laza/js/products.js
@@ -34,7 +34,7 @@ function displayProduct(productData) {
         let product = productData[id];
         elem +=
             `<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 p-2 ">
-                <a href="html/detail.html?id=${id}" target="_blank">
+                <a href="/laza/html/detail.html?id=${id}" target="_blank">
                     <img class="img-fluid img-thumbnail" src="${product.img[0]}" alt="product image" />
                     <p class="text-center no-decoration">${product.name}</p>
                     <p class="text-center price no-decoration">${product.price}</p>
@@ -49,7 +49,7 @@ function displayProduct(productData) {
 function managePaginationAppearance() {
     let url = window.location.href;
     let page = parseInt(url.split("=")[1]);
-
+    if (!page) page = 1;
     let pageElements = document.getElementsByClassName('page-item');
     let lastPage = pageElements.length - 2;
     // console.log(pageElements);
