66fd2b7099140cb128845c31fc2ea4cdaba384c0
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-06-17 16:56:26 +0700

    thang-long-tour: build lading page

--- thang-long-tour/js/script.js
+++ thang-long-tour/js/script.js
@@ -3,3 +3,6 @@ function dynamicallyLoadScript(url) {
     script.src = url; //Set it's src to the provided URL
     document.body.appendChild(script); //Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
+
+// Add js for responsive top nav
+dynamicallyLoadScript('../lib/top-nav.js')
