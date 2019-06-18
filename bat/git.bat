10c2d0ac4dc1616f9f33a450535086efc2eeeed7
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-06-18 10:30:37 +0700

    thang-long-tour: add zalo, facebook chat

--- bat/git.bat
+++ bat/git.bat
@@ -1,4 +1,4 @@
 cd F:\manh-trinhquoc.github.io
 git add .
-git commit -m "cacl: fix bug programme infinite loop when click +-*/% button first"
+git commit -m "thang-long-tour: add zalo, facebook chat"
 git push
