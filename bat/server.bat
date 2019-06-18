0151b38ae0a4891143d6ee341f358347fbf05b2f
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-06-18 14:35:40 +0700

    thang-long-tour: add zalo, facebook chat

--- bat/server.bat
+++ bat/server.bat
@@ -1,6 +1,9 @@
 cd F:\manh-trinhquoc.github.io
 start http://localhost:8000/thang-long-tour
 start https://www.figma.com/file/dvRgJ2wVzA1gYuE4LWjVouHt/Thanglong
+start  "" "C:\Program Files\Mozilla Firefox\firefox.exe" "https://manh-trinhquoc.github.io/"
+start  "" "C:\Users\Manh-Admin\AppData\Local\CocCoc\Browser\Application\browser.exe" "https://manh-trinhquoc.github.io/"
+start  "" "C:\Users\Manh-Admin\Desktop\Microsoft Edge.lnk" https://manh-trinhquoc.github.io/
 python -m http.server 8000
 
 
