6082bacc7304870079c2d38e2e945f251f99e21f
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-07-02 18:47:08 +0700

    thang-long-tour: change html file name

--- bat/st.bat
+++ bat/st.bat
@@ -1,5 +1,4 @@
 start  "" "C:\Program Files\Sublime Text 3\sublime_text.exe" "F:\manh-trinhquoc.github.io"
-start  "" "C:\Program Files\Sublime Text 3\subl.exe" "F:\manh-trinhquoc.github.io\thang-long-tour\hotel-detail.html"
 start  "" "C:\Program Files\Sublime Text 3\subl.exe" "F:\manh-trinhquoc.github.io\thang-long-tour\scss\common.scss"
 start  "" "C:\Program Files\Sublime Text 3\subl.exe" "F:\manh-trinhquoc.github.io\thang-long-tour\scss\detail.scss"
 start  "" "C:\Program Files\Sublime Text 3\subl.exe" "F:\manh-trinhquoc.github.io\thang-long-tour\scss\find.scss"
