d3ae2fad52a7f487dc284bc49b0f00552fae4425
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-07-02 21:33:16 +0700

    thang-long-tour: add some action to fix facebook plugin. no success so far

--- bat/git.bat
+++ bat/git.bat
@@ -1,4 +1,4 @@
 cd F:\manh-trinhquoc.github.io
 git add .
-git commit -m "thang-long-tour: change html file name"
+git commit -m "thang-long-tour: add some action to fix facebook plugin. no success so far"
 git push
