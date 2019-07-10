1a1aa62f50afdb40adcea503912a9fe444e3d522
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-07-10 16:53:29 +0700

    thang-long-tour: delete preload.js

--- bat/git.bat
+++ bat/git.bat
@@ -1,4 +1,4 @@
 cd F:\manh-trinhquoc.github.io
 git add .
-git commit -m "thang-long-tour: add preload.js"
+git commit -m "thang-long-tour: delete preload.js"
 git push
--- thang-long-tour/js/lib.js
+++ thang-long-tour/js/lib.js
@@ -1,3 +1,14 @@
+function dynamicallyLoadScript(url, defer = true, async = false) {
+    var script = document.createElement("script"); //Make a script DOM node
+    script.src = url; //Set it's src to the provided URL
+    if (defer) script.setAttribute('defer', '');
+    if (async) script.setAttribute('async', '');
+    document.head.appendChild(script); //Add it to the end of the head section of the page
+}
+
+// Add responsive top nav library
+dynamicallyLoadScript('/thang-long-tour/comp/top-nav.js')
+
 // Các hàm bật/tắt popover
 let eleTopnavPopover = document.getElementById("topnav");
 
