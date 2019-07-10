59d00ba24dd054d4e342a79646fde9d12ba3c607
manh-trinhquoc <34735335+manh-trinhquoc@users.noreply.github.com>
2019-07-10 12:19:04 +0700

    thang-long-tour: add preload.js

--- bat/git.bat
+++ bat/git.bat
@@ -1,4 +1,4 @@
 cd F:\manh-trinhquoc.github.io
 git add .
-git commit -m "thang-long-tour: finish find-hotels.js"
+git commit -m "thang-long-tour: add preload.js"
 git push
--- thang-long-tour/js/lib.js
+++ thang-long-tour/js/lib.js
@@ -1,12 +1,3 @@
-function dynamicallyLoadScript(url) {
-    var script = document.createElement("script"); //Make a script DOM node
-    script.src = url; //Set it's src to the provided URL
-    document.body.appendChild(script); //Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
-}
-
-// Add responsive top nav
-dynamicallyLoadScript('/thang-long-tour/comp/top-nav.js');
-
 // Các hàm bật/tắt popover
 let eleTopnavPopover = document.getElementById("topnav");
 
